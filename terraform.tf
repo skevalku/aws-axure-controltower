2747772a0d22e3a2707ab451af162cae2fa6a333
Kevalkumar Sheth <110819382+skevalku@users.noreply.github.com>
2023-02-20 20:37:06 +0000

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,8 @@
-/*terraform {
+terraform {
   backend "s3" {
-    bucket = "terraform-petarskevalkutest-state"
+    bucket = "terraform-skevalku-state-123"
     region = "eu-central-1"
     key    = "dc/s3/terraform.tfstate"
     dynamodb_table = "tf-locks"
  }
 }
-*/
