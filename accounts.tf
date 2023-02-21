6505b44e1dfe014603e30886fe2b94968b179f81
EC2 Default User <ec2-user@ip-172-31-28-111.eu-west-1.compute.internal>
2023-02-21 12:53:39 +0000

    changed S3 config file

--- accounts.tf
+++ accounts.tf
@@ -45,6 +45,40 @@ resource "aws_servicecatalog_provisioned_product" "accounts" {
   ignore_errors = "true"
 }
 
+resource "aws_s3_bucket" "terraform_state_s3" {
+  bucket        = "${var.managed_resource_prefix}-state"
+  force_destroy = false
+}
+
+resource "aws_s3_bucket_versioning" "s3_versioning_tf_state" {
+  bucket = aws_s3_bucket.terraform_state_s3.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse_tf_state" {
+  bucket = aws_s3_bucket.terraform_state_s3.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "${var.managed_resource_prefix}-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+
 /*resource "aws_ce_cost_category" "test2" {
 name = "Newtes"
 rule_version = "CostCategoryExpression.v1"
--- variables.tf
+++ variables.tf
@@ -79,35 +79,3 @@ variable "ou_mapping_table" {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state_s3" {
-  bucket        = "${var.managed_resource_prefix}-state"
-  force_destroy = false
-}
-
-resource "aws_s3_bucket_versioning" "s3_versioning_tf_state" {
-  bucket = aws_s3_bucket.terraform_state_s3.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_sse_tf_state" {
-  bucket = aws_s3_bucket.terraform_state_s3.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "${var.managed_resource_prefix}-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
